4bb820c927c36defa3c09813beed9c6374e893ed
Asker <hms467@econ.ku.dk>
2020-03-02 20:50:01 +0100

    PS3

--- PS2/mymodule.py
+++ PS2/mymodule.py
@@ -3,6 +3,14 @@ def myfun(n):
         print('hello world!')
 
 def myfun2(n):
+    '''
+    This function prints "Goodbye" n times
+
+    Args:
+
+        n (int): numbers of times to print Goodbye 
+    
+    '''
     for i in range(n):
         print('Goodbye')
 
